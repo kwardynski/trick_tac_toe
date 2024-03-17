c26ee8ac687de5bfde406716d8dd114cc641bc52
Kacper Wardynski <kacper.wardynski@gmail.com>
2024-03-17 15:10:58 -0600

    game works

--- lib/trick_tac_toe/game/states.ex
+++ lib/trick_tac_toe/game/states.ex
@@ -6,7 +6,7 @@ defmodule TrickTacToe.Game.States do
   def transition(:player_one_turn, :continue), do: {:ok, :player_two_turn}
   def transition(:player_two_turn, :continue), do: {:ok, :player_one_turn}
 
-  def transition(_, :draw), do: {:ok, :end_game}
+  def transition(_, :draw), do: {:ok, :draw}
   def transition(_, :player_one_win), do: {:ok, :player_one_win}
   def transition(_, :player_two_win), do: {:ok, :player_two_win}
 
@@ -14,6 +14,7 @@ defmodule TrickTacToe.Game.States do
 
   def transition(:player_one_win, _), do: {:error, :end_game}
   def transition(:player_two_win, _), do: {:error, :end_game}
+  def transition(:draw, _), do: {:error, :end_game}
 
   def transition(_, _), do: {:error, :unknown_state}
 end
